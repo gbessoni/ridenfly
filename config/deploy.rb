ee150e73e7a522f851bfc4f5b497b021f189ba53
Jacek Hiszpanski <jacek.hiszpanski@naturaily.com>
2014-10-31 13:49:18 +0100

    Add repository for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'ridenfly'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:shuttlefare/ridenfly.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
