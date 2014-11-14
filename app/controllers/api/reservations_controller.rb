a3d462e568652e3411ad9ee54a65173bcf8820e6
Jacek Hiszpanski <jacek.hiszpanski@naturaily.com>
2014-11-14 09:23:56 +0100

    Update reservation allowed params in api

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -49,7 +49,7 @@ class Api::ReservationsController < Api::ApplicationController
       params.require(:reservation).permit(
         :airport_id, :company_id, :flight_datetime, :pickup_datetime, :passenger_name,
         :phone, :num_of_passengers, :net_fare, :gratuity, :addresss, :cross_street,
-        :airline, :luggage, :cancelation_reason, :flight_number, :status, :service_type
+        :airline, :luggage, :flight_number, :service_type
       )
     end
 end
