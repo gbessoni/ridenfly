0656d28e53d18f5685d6f5c247c8e16ea5444e03
Jacek Hiszpanski <jacek.hiszpanski@naturaily.com>
2014-12-01 15:34:11 +0100

    Setup prod depoyment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,10 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
+role :app, %w{104.130.50.22}
+role :web, %w{104.130.50.22}
+role :db,  %w{104.130.50.22}
 
 # Extended Server Syntax
 # ======================
@@ -15,8 +14,9 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}
+server '104.130.50.22', user: 'deployer', roles: %w{web app db}
 
+set :rails_env, "production"
 
 # Custom SSH Options
 # ==================
