3bcaee4fbb9a2551af873f230dd4d358facb1ade
Konstantin Rafalsky <kraft001@gmail.com>
2019-03-12 12:00:32 +0300

    Update deployment env configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,46 +1,4 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
-
-role :app, %w{136.243.88.42}
-role :web, %w{136.243.88.42}
-role :db,  %w{136.243.88.42}
-
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server '136.243.88.42', user: 'deployer', roles: %w{web app}
+server '136.243.88.42', user: 'deployer', roles: %w{web app db}
 
 set :rails_env, "staging"
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
