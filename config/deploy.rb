aa28bb8b17659d8d7fae2432eddf8f6fe80fd13a
Konstantin Rafalsky <kraft001@gmail.com>
2019-03-12 12:12:04 +0300

    Update the repo url in the deployemnt config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'ridenfly'
-set :repo_url, 'git@github.com:shuttlefare/ridenfly.git'
+set :repo_url, 'git@bitbucket.org:kraft001/ridenfly.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
