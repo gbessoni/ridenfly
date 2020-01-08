da45c91b767369d5e7c7109b6b5caac22c3d392f
Konstantin Rafalsky <kraft001@gmail.com>
2020-01-08 10:19:20 +0300

    Fix the company commission misprint

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -51,7 +51,7 @@ class Reservation < ActiveRecord::Base
   end
 
   def amount_due
-    total_net_fare * (100 - company.comission.to_f)/100
+    total_net_fare * (100 - company.commission.to_f)/100
   end
 
   def cancel(params)
